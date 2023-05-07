cd8d44a2a803e3758d53b73cc1e777df5401b800
Neculaiu Cosmin <cosminflorinel23@gmail.com>
2023-05-07 21:44:50 +0300

    setup as activity

--- CatalogAdmin/Orar/MainActivity.cs
+++ CatalogAdmin/Orar/MainActivity.cs
@@ -1,3 +1,6 @@
+using Android.Content;
+using Android.Preferences;
+
 namespace Orar;
 
 [Activity(Label = "@string/app_name", MainLauncher = true)]
@@ -9,5 +12,31 @@ public class MainActivity : Activity
         
         // Set our view from the "main" layout resource
         SetContentView(Resource.Layout.activity_main);
+
+
+
+    }
+
+    // protected override void OnStart()
+    // {
+    //     base.OnStart();
+    //     if (IsSetupNeeded())
+    //     { 
+    //         var intent = new Intent(this, typeof(SetupActivity));
+    //         StartActivity(intent);
+    //     }
+    // }
+
+    private bool IsSetupNeeded()
+    {
+        ISharedPreferences? sharedPreferences = PreferenceManager.GetDefaultSharedPreferences(this);
+        string grp =  sharedPreferences.GetString("grp", string.Empty);
+        string year = sharedPreferences.GetString("year", string.Empty);
+        if (!string.IsNullOrEmpty(grp)&&!string.IsNullOrEmpty(year))
+        {
+            return false;
+        }
+
+        return true;
     }
 }
